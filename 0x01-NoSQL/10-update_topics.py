6d28b85ee19d4ee39f50d13c2a1f8c78b164428b
oangel26 <oa.angel26@gmail.com>
2022-08-16 22:53:25 -0500

    first commit

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Change school topics
+"""
+
+
+from pymongo import MongoClient
+list_all = __import__('8-all').list_all
+
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Function that changes all topics of a school document based on the name
+    name (string) will be the school name to update
+    topics (list of strings) will be the list of topics approached in the school
+    """
+    result = mongo_collection.update_one({'name': name}, {'$set': {'topics': topics}})
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    school_collection = client.my_db.school
+    update_topics(school_collection, "Holberton school",
+                  ["Sys admin", "AI", "Algorithm"])
+
+    schools = list_all(school_collection)
+    for school in schools:
+        print("[{}] {} {}".format(school.get('_id'),
+                                  school.get('name'),
+                                  school.get('topics', "")))
+
+    update_topics(school_collection, "Holberton school", ["iOS"])
+
+    schools = list_all(school_collection)
+    for school in schools:
+        print("[{}] {} {}".format(school.get('_id'),
+                                  school.get('name'),
+                                  school.get('topics', "")))
